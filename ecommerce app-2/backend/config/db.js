a6b5e881701b92e52441ae9c67fcb81c23bc6e18
Livanthika <livanthika.02@gmail.com>
2026-09-14 17:21:17 +0530

    use standard mongodb connection string

--- ecommerce app-2/backend/config/db.js
+++ ecommerce app-2/backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb+srv://livanthika02_db_user:Livan1302@cluster0.kf4m.mongodb.net/ecommerce?retryWrites=true&w=majority"
+      process.env.MONGO_URI || "mongodb://livanthika02_db_user:Livan1302@ac-2xp1mpm-shard-00-00.6zpivgt.mongodb.net:27017,ac-2xp1mpm-shard-00-01.6zpivgt.mongodb.net:27017,ac-2xp1mpm-shard-00-02.6zpivgt.mongodb.net:27017/ecommerce?ssl=true&replicaSet=atlas-lvydf8-shard-0&authSource=admin"
     );
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
